1bdcca382f45155999e34d48484b7c0ab4816505
hjun0325 <100742383+hjun0325@users.noreply.github.com>
2025-09-17 17:03:07 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+
+	int N;
+	int x;
+
+	std::cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		std::cin >> x;
+		if (x == 0)
+		{
+			if (pq.size() == 0)
+			{
+				std::cout << "0\n";
+			}
+			else
+			{
+				std::cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(x);
+		}
+	}
+
+	return 0;
+}
